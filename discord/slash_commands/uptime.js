5701b13a79f09e3a446bc937b7018ba2966a9cde
raunakrajadh <raunakrajadh@gmail.com>
2022-05-23 00:09:43 +0545

    uptime added to slashcommands

---
+++ discord/slash_commands/uptime.js
@@ -0,0 +1,25 @@
+const { SlashCommandBuilder } = require('@discordjs/builders')
+
+module.exports = {
+    data: new SlashCommandBuilder()
+        .setName('uptime')
+        .setDescription('Bots uptime!'),
+
+    async execute(Discord, client, interaction){
+
+        let totalSeconds = (client.uptime / 1000);
+        let days = Math.floor(totalSeconds / 86400);
+        totalSeconds %= 86400;
+        let hours = Math.floor(totalSeconds / 3600);
+        totalSeconds %= 3600;
+        let minutes = Math.floor(totalSeconds / 60);
+        let seconds = Math.floor(totalSeconds % 60);
+        
+        let uptime = `${days} days, ${hours} hours, ${minutes} minutes and ${seconds} seconds`;
+        
+        const embed = new Discord.MessageEmbed()
+            .setColor('BLUE')
+            .setDescription(uptime)
+        await interaction.reply({embeds: [embed], ephemeral: true})
+    }
+}
