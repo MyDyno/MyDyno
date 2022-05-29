06d9ecfaa3a6715543088046cb91df175a0cdbfa
raunakrajadh <raunakrajadh@gmail.com>
2022-05-30 00:33:56 +0545

    updates

--- discord/commands/games/blackjack.js
+++ discord/commands/games/blackjack.js
@@ -6,6 +6,26 @@ module.exports = {
     cooldown: 5 * 1000,
 
     async execute(Discord, client, message, args, PREFIX){
+
+        // let game = await blackjack(message)
+        
+        // switch (game.result) {
+            
+        //     case "WIN":
+        //         message.channel.send({embeds: [
+        //             new Discord.MessageEmbed()
+        //             .setTitle("You won!")
+        //         ]})
+        //         break;
+        //     case "LOSE":
+        //         message.channel.send({embeds: [
+        //             new Discord.MessageEmbed()
+        //             .setTitle("You lost!")
+        //         ]})
+        //     break;
+            
+        // }
+
         blackjack(message)
         //https://discord-blackjack.gitbook.io/discord-blackjack/examples
     }        
