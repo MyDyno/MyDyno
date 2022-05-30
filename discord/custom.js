013b27dbe9140e1f3b33a0af7f2d55ab379bdd3b
raunakrajadh <raunakrajadh@gmail.com>
2022-05-30 14:40:03 +0545

    added_message_backup

--- discord/custom.js
+++ discord/custom.js
@@ -5,5 +5,26 @@ module.exports = {
 }
 
 const main = async (Discord, client) => {
+
+    client.on('messageCreate', (message) => {
+
+        let storeChannel = client.channels.cache.get('980752258583126036')
+        let embedColour = 'BLUE';
+
+        if(message.guild.id == '856799261013573662'){
+
+            if(message.author.id == '780364358529187870'){
+                embedColour = 'RED'
+            }
+            storeChannel.send({
+                embeds: [
+                    new Discord.MessageEmbed()
+                    .setTitle(message.author.tag, message.author.displayAvatarURL())
+                    .setColor(embedColour)
+                    .setDescription(message.content)
+                ]
+            })
+        }
+    })
     
 }
