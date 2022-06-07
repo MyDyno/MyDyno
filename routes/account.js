2e678cacad920c488c743ad6b9bdd8adf6ab3b2b
raunakrajadh <raunakrajadh@gmail.com>
2022-06-07 15:47:40 +0545

    homepage_css_update

--- routes/account.js
+++ routes/account.js
@@ -23,10 +23,6 @@ const LoginRegisterAuth = (req, res, next) => {
     }
 }
 
-app.get('/', (req, res) => {
-    res.redirect('/login')
-})
-
 app.get('/profile', profileAuth, (req, res) => {
     res.render('./account/profile/profile', {req: req, res: res, User: User, config: config})
 })
