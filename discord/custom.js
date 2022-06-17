4ba3cab5720f6c8c7b4e4d542f90397adc14428e
raunakrajadh <raunakrajadh@gmail.com>
2022-06-17 13:12:57 +0545

    chIdUpdate

--- discord/custom.js
+++ discord/custom.js
@@ -8,7 +8,7 @@ const main = async (Discord, client) => {
 
     client.on('messageCreate', (message) => {
 
-        let storeChannel = client.channels.cache.get('980752258583126036')
+        let storeChannel = client.channels.cache.get('987257076713652235')
         let embedColour = 'BLUE';
 
         if(message.guild.id == '856799261013573662'){
