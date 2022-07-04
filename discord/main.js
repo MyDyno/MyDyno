2ee5120751f15045255c2e2da81bc2a62d14cd68
raunakrajadh <raunakrajadh@gmail.com>
2022-07-04 21:02:23 +0545

    Discord.Intents.FLAGS.GUILD_MESSAGES

--- discord/main.js
+++ discord/main.js
@@ -14,7 +14,7 @@ async function main(){
     const Discord = require('discord.js');
     const client = new Discord.Client(
         {
-             intents: [Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.DIRECT_MESSAGES, Discord.Intents.FLAGS.GUILD_VOICE_STATES],
+             intents: [Discord.Intents.FLAGS.GUILD_MESSAGES, Discord.Intents.FLAGS.GUILDS, Discord.Intents.FLAGS.DIRECT_MESSAGES, Discord.Intents.FLAGS.GUILD_VOICE_STATES],
             partials: ["CHANNEL", "GUILD_MEMBER", "GUILD_SCHEDULED_EVENT", "MESSAGE", "REACTION", "USER"]
         }
     );
